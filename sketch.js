3051db67c8d99cc62cd534375072fa8493f929d6
PrinceSoumya <72249417+PrinceSoumya@users.noreply.github.com>
2020-11-26 18:11:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+var ground,paperObject,dustbinObject,ima;
+
+function setup() {
+	var canvas=createCanvas(1600, 700);
+    engine = Engine.create();
+	world = engine.world;
+
+  //Create the Bodies Here.
+  dustbinObject = new dustbin(1100,670,300,300);
+ paperObject = new paper(200,450,30)
+  ground = new grounds(800,680,1700,20)
+
+	Engine.run(engine);
+	
+	
+
+   
+}
+
+
+function draw() {
+ 
+  rectMode(CENTER);
+  background("lightgrey");
+  
+
+  paperObject.display();
+  ground.display();
+ dustbinObject.display();
+  //KeyPressed();
+  
+ 
+}
+
+function keyPressed(){
+ if (keyCode === UP_ARROW){
+Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:45,y:-55});
+
+  }
+}
+
